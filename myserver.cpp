31d56e606b9acb4362c9b653fc85b7095f51a88b
Astrgan <astrgan@gmail.com>
2016-03-13 11:29:04 +1000

    fiks

--- myserver.cpp
+++ myserver.cpp
@@ -1,8 +1,9 @@
 #include "myserver.h"
+#include <QTextCodec>
 
 MyServer::MyServer(QObject *parent) : QTcpServer(parent)
 {
-    QFile file("D:\\videPath.txt");
+    QFile file("D:\\videPaths.txt");
     if(file.open(QIODevice::ReadOnly | QIODevice::Text)){
         QTextStream readStream(&file);
         while (!readStream.atEnd()) {
@@ -31,10 +32,10 @@ void MyServer::startServer()
 
 void MyServer::saveString()
 {
-    QFile file("D:\\videPath.txt");
+    QFile file("D:\\videPaths.txt");
     if(file.open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Append)){
         QTextStream writeStream(&file);
-        writeStream << "\r\n" << listVideoPath.last();
+        writeStream << listVideoPath.last() <<"\n";
 
         file.close();
     }
@@ -42,6 +43,11 @@ void MyServer::saveString()
 
 
 void MyServer::processing(QByteArray request){
+
+
+    /*************ВЕРСИЯ-2*************/
+
+    /*************ВЕРСИЯ-1*************/
 
     QVector<QChar> requestVec;
     QDataStream streamOUT(request);
@@ -56,32 +62,53 @@ void MyServer::processing(QByteArray request){
     case 1:
         qDebug()<<"case1";
         foreach(QString videoPath, listVideoPath){
-            streamAnswer << videoPath << "\r\n";
+            streamAnswer << videoPath << QString("\n");
         }
        break;
     case 2:
-        listVideoPath.append(QString(requestVec.mid(1,-1).begin(), requestVec.size()-1));
+        listVideoPath.append(QString (requestVec.mid(1,-1).begin(), requestVec.size()-1));
         saveString();
         qDebug() << "listVideoPath.last()" << listVideoPath.last();
-        streamAnswer << listVideoPath.last() << "\r\n";
+        streamAnswer << listVideoPath.last() << QString("\n");
         break;
+
     }
 
     /*ТЕСТИРОВАНИЕ HTTP ЗАПРОСА*/
 /*
     QByteArray answer;
     QDataStream streamAnswer(&answer, QIODevice::WriteOnly);
-    streamAnswer << "HTTP/1.0 200 Ok\r\n"
-          "Content-Type: text/html; charset=\"utf-8\"\r\n"
-          "\r\n"
+    streamAnswer << "HTTP/1.0 200 Ok\n"
+          "Content-Type: text/html; charset=\"utf-8\"\n"
+          "\n"
           "<h1>Nothing to see here</h1>\n"
           << QDateTime::currentDateTime().toString() << "\n";
     qDebug()<<request;
-*/
 
+*/
     /*КОНЕЦ ТЕСТИРОВАНИЕ HTTP ЗАПРОСА*/
 
+    /*ТЕСТИРОВАНИЕ HTTP ЗАПРОСА222*/
+/*
+    QByteArray header;
+    QDataStream streamAnswer(&header, QIODevice::WriteOnly);
+    streamAnswer << "HTTP/1.0 200 Ok\n"
+          "Content-Type: application/JNLPtest2.jar\n"
+          "\n";
 
+    emit sendAll(header);
+
+
+    QByteArray answer;
+    QFile file("D:\\Users\\Astrgan\\Documents\\Development\\WorkspaceForMyJob\\JNLPtest2\\dist\\JNLPtest2.jar");
+    if(file.open(QIODevice::ReadOnly)){
+        answer=file.readAll();
+    }
+    file.close();
+        qDebug()<<request;
+*/
+
+        /*КОНЕЦ ТЕСТИРОВАНИЕ HTTP ЗАПРОСА222*/
     emit sendAll(answer);
 }
 
